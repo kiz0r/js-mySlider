78dcb904823d7084064ebe88953e92fc8fb3722a
Kirill Khomenko <kkhomenko10@gmail.com>
2023-01-08 12:24:31 +0100

    change image url

--- js/main.js
+++ js/main.js
@@ -2,32 +2,32 @@
 
 const mySlides = [
   {
-    src: './../img/place1.jpg',
+    src: './img/place1.jpg',
     alt: 'place1',
     description: 'At first I will go threre. Its my hometown.',
   },
   {
-    src: './../img/place2.jpg',
+    src: './img/place2.jpg',
     alt: 'place2',
     description: 'Its a Ukrainian city too. Its called Bukovel.',
   },
   {
-    src: './../img/place3.jpg',
+    src: './img/place3.jpg',
     alt: 'place3',
     description: 'Its the place in Dubai and I would like to visit it.',
   },
   {
-    src: './../img/place4.jpg',
+    src: './img/place4.jpg',
     alt: 'place4',
     description: 'Its a Spanish landscape, I would like to visit it too.',
   },
   {
-    src: './../img/place5.jpeg',
+    src: './img/place5.jpeg',
     alt: 'place5',
     description: 'Its in Maldives.',
   },
   {
-    src: './../img/place6.jpg',
+    src: './img/place6.jpg',
     alt: 'place6',
     description: 'Its London, the capital of the UK.',
   },
@@ -38,13 +38,13 @@ const mySlides = [
       'This place I would like to visit is located in the Netherlands.',
   },
   {
-    src: './../img/place8.jpeg',
+    src: './img/place8.jpeg',
     alt: 'place8',
     description:
       'I will buy the most expensive tickets to visit burning Moscow.',
   },
   {
-    src: './../img/place9.jpg',
+    src: './img/place9.jpg',
     alt: 'place9',
     description: 'Its the Bubblegum Lake which is located in Australia',
   },
@@ -92,8 +92,3 @@ function updateSlide(index) {
       'https://st.depositphotos.com/2074779/2629/i/600/depositphotos_26292095-stock-photo-question-mark.jpg';
   };
 }
-
-// 4) Додати слайду опис (description).
-// Advanced:
-// Доробити анімацію появи наступного кадру в слайдері або додати з боків від основної картинки попередню та наступну за схемою.
-// Приклад розташування 3-х картинок у слайдері у прикріпленому файлі.
